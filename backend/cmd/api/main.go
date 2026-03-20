adabd8b9ff147c31532b9c65d17c0c86b7b02ccc
akshatgupta15 <iitkakshat@gmail.com>
2026-03-20 20:33:34 +0530

    feat: update CORS configuration to allow all origins

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -30,7 +30,8 @@ func main() {
 	app.Use(logger.New())
 	// app.Use(cors.New(cors.Config{AllowOrigins: "http://localhost:3000"}))
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000",
+		// AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     "*",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
