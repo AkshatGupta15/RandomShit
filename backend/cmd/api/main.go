5f37aec65783d74f280473ef8d0e8658fd3487fe
akshatgupta15 <iitkakshat@gmail.com>
2026-03-22 02:54:16 +0530

    feat: Update CORS configuration to allow requests from the production domain

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -39,7 +39,7 @@ func main() {
 	// app.Use(cors.New(cors.Config{AllowOrigins: "http://localhost:3000"}))
 	app.Use(cors.New(cors.Config{
 		// AllowOrigins:     "http://localhost:3000",
-		AllowOrigins:     "http://localhost:3000,https://your-app.pages.dev",
+		AllowOrigins:     "http://localhost:3000,https://randomshit-1.onrender.com",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
